153efd612115e9db59a12a74c2bfe0e070388a5e
Carla Gonçalves <carla.goncalves@onfido.com>
2021-01-02 19:41:59 +0000

    Removed useless dependency

--- dist/_nuxt/static/1609616406/contacts/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fandreialu\u002F_nuxt\u002Fstatic\u002F1609616406",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{sidebar:b,title:b},serverRendered:false,routePath:a,config:{}}}("\u002Fcontacts",""));
