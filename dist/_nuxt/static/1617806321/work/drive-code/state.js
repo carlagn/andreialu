c5e227f1ed0fc7370c45c2d4564587430dbe90f5
Carla Gonçalves <carla.goncalves@onfido.com>
2021-04-07 15:55:24 +0100

    another deploy?

--- dist/_nuxt/static/1617806321/work/drive-code/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fandreialu\u002F_nuxt\u002Fstatic\u002F1617806321",layout:"default",error:null,state:{sidebar:a,title:a,activePage:"work"},serverRendered:true,routePath:"\u002Fwork\u002Fdrive-code",config:{}}}(""));
