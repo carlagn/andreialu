e159b60c1c473b86853f2fa14d1767e8547e4354
Carla Gonçalves <carla.goncalves@onfido.com>
2021-05-25 16:27:33 +0100

    Dist update

--- dist/_nuxt/static/1621956339/work/style-guide/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621956339",layout:"default",error:null,state:{sidebar:a,title:a,activePage:"work",activeZoom:a},serverRendered:true,routePath:"\u002Fwork\u002Fstyle-guide",config:{}}}(""));
